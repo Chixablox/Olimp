2f995684fad380dfcc827c64ce32e57d3a5324a3
Alexander <125283933+Chixablox@users.noreply.github.com>
2023-04-27 12:47:42 +0600

    Update Формы.cs

--- Формы.cs
+++ Формы.cs
@@ -8,103 +8,111 @@ namespace Delegate
     {
         static void Main(string[] args)
         {
-            StreamReader sr = new StreamReader("input_s1_01.txt");
+
+            //Считывание данных
+            StreamReader sr = new StreamReader("input2.txt");
+            StreamReader sr1 = new StreamReader("output2.txt");
             string s = sr.ReadLine();
             int n = Convert.ToInt32(s);
-            List<int[]> det = new List<int[]>();
+            List<string[]> det = new List<string[]>();
             for (var i = 0; i < n; i++)
             {
                 s = sr.ReadLine();
-                int[] d1 = new int[20];
-                for (var j = 0; j < 20; j++)
-                {
-                    d1[j] = Convert.ToInt32(s[j]);
-                }
+                string[] d1 = s.Split(' ');
                 det.Add(d1);
             }
-            List<int[]> pol = new List<int[]>();
-            for (var i = 0; i < 2*n; i++)
-            {
-                s = sr.ReadLine();
-                int[] d1 = new int[15];
-                for (var j = 0; j < 15; j++)
-                {
-                    d1[j] = Convert.ToInt32(s[j]);
-                }
-                pol.Add(d1);
-            }
-            List<int> otvet = new List<int>();
+            List<string[]> pol = new List<string[]>();
             for (var i = 0; i < 2 * n; i++)
             {
-                if (pol[i][0] == 2) continue;
-                for (var j = 0; j < n; j++)
+                s = sr.ReadLine();
+                string[] d1 = s.Split(' ');
+                pol.Add(d1);
+            }
+
+            //Решение задачи
+            List<List<int>> otvet = new List<List<int>>();
+            for (var i = 0; i < det.Count; i++)
+            {
+                List<int> d1 = new List<int>();
+                otvet.Add(d1);
+            }
+                for (var i = 0; i < pol.Count; i++)
                 {
-                    if (det[j][0] == 2) continue;
-                    int sov = 0;
-                    for (var l = 0; l < 5; l++)
+                    for (var j = 0; j < det.Count; j++)
                     {
-                        if (pol[i][l] == det[j][l])
+                        int ch = 0;
+                        for (var k = 0; k < 5; k++)
                         {
-                            sov++;
-                        }
-                    }
-                    for (var l = 5; l < 10; l++)
-                    {
-                        if (pol[i][l] == det[j][l])
-                        {
-                            sov++;
-                        }
-                    }
-                    for (var l = 5; l < 10; l++)
-                    {
-                        if (pol[i][l] == det[j][l+5])
-                        {
-                            sov++;
-                        }
-                    }
-                    for (var l = 10; l < 15; l++)
-                    {
-                        if (pol[i][l] == det[j][l + 5])
-                        {
-                            sov++;
-                        }
-                    }
-                    int polov = 0;
-                    for (var k = 0; k < 2 * n; k++)
-                    {
-                        if (k == i) continue;
-                        for (var l = 0; l < 5; l++)
-                        {
-                            if (pol[i][l] == pol[k][l])
+                            if ((pol[i][k] == det[j][k]) || (pol[i][k] == det[j][4 - k]))
                             {
-                                polov++;
+                                ch++;
                             }
                         }
-                        for (var l = 10; l < 15; l++)
+                        if (ch != 5) continue;
+                        for (var k = 10; k < 15; k++)
                         {
-                            if (pol[i][l] == pol[k][l])
+                            if ((pol[i][k] == det[j][k]) || (pol[i][k] == det[j][14 - k]))
                             {
-                                polov++;
+                                ch++;
                             }
                         }
-                        if ((sov == 15) && (polov == 10))
+                        if (ch != 10) continue;
+                        for (var k = 5; k < 10; k++)
                         {
-                            det[j][0] = 2;
-                            pol[i][0] = 2;
-                            pol[k][0] = 2;
-                            otvet.Add(j);
-                            otvet.Add(k);
-                            continue;
+                            if ((pol[i][k] == det[j][k]) || (pol[i][k] == det[j][9 - k + 5]))
+                            {
+                                ch++;
+                            }
+                        }
+                       // Console.WriteLine(ch);
+                        if (ch < 15)
+                        {
+                            ch = 10;
+                            for (var k = 5; k < 10; k++)
+                            {
+                                if ((pol[i][k] == det[j][k + 10]) || (pol[i][k] == det[j][19 - k]))
+                                {
+                                    ch++;
+                                }
+                            }
+                        }
+
+                        if (ch == 15)
+                        {
+                            otvet[j].Add(i + 1);
                         }
                     }
                 }
-            }
-            for (var i = 0; i < otvet.Count; i++)
-            {
-                if(i%2!=0)Console.Write(otvet[i] + " ");
-                else Console.WriteLine(otvet[i]);
-            }
+
+
+            //Вывод ответа
+                Console.WriteLine("Мой ответ");
+                for (var i = 0; i < otvet.Count; i++)
+                {
+                    for (var j = 0; j < otvet[i].Count; j++) 
+                    { 
+                        Console.Write(otvet[i][j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                List<string[]> otvetreal = new List<string[]>();
+                for (var i = 0; i < n; i++)
+                {
+                    s = sr1.ReadLine();
+                    string[] d1 = s.Split(' ');
+                    otvetreal.Add(d1);
+                }
+                Console.WriteLine("Реальный ответ");
+                for (var i = 0; i < det.Count; i++)
+                {
+                    for (var j = 0; j < 2; j++)
+                    {
+                        Console.Write(otvetreal[i][j] + " ");
+                    }
+                    Console.WriteLine();
+                }
             Console.ReadLine();
+
         }
     }
 }
